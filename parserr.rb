b6d620a80e6f4a5828008c23c1f0476eca489996
ArsenyBuntury <yeahwheel@gmail.com>
2023-01-05 15:36:12 +0300

    Fix #1

--- parserr.rb
+++ parserr.rb
@@ -2,30 +2,32 @@ require 'curb'
 require 'nokogiri'
 require 'csv'
 
-url = "https://www.petsonic.com/farmacia-para-gatos/"
+url = ARGV.first
+file_name =  ARGV.last
 html = Curl.get(url)
 main_category_page = Nokogiri::HTML(html.body)
+
 all_products_url = main_category_page.xpath("//div/div/link[@itemprop='url']/@href")
+all_products_url.each do |product_url|
+  html = Curl.get(product_url)
+product_page = Nokogiri::HTML(html.body)
+ 
+headers = %w[Название Цена Изображение]
 
+CSV.open("#{file_name}.csv", "a+") do |csv_line|
+  csv_line << headers if csv_line.count.zero?
+end
 
+product_name = product_page.xpath('//h1[@class="product_main_name"]').text
+product_img = product_page.xpath("//div/span/img[@id='bigpic']/@src").text
+product_price = product_page.xpath("//span[@class='price_comb']")
+product_variant = product_page.xpath("//span[@class='radio_label']")
+product_variant.each_with_index do |v , index|
+CSV.open("#{file_name}.csv", "a") do |csv_line|
 
-CSV.open('parsed.csv', "a") do |csv_line|
-  csv_line << ['Название', 'Весовка', 'Цена']
-
-  
-  all_products_url.each do |product_url|
-    html = Curl.get(product_url)
-    product_page = Nokogiri::HTML(html.body)
-    product_name = product_page.xpath('//h1[@class="product_main_name"]').text
-    attribute_product_list = product_page.xpath('//div[@class="attribute_list"]')
-    attribute_product_list.each do |attr|
-      product_variant = product_page.xpath('//span[@class="radio_label"]').text
-
-      product_price = product_page.xpath('//span[@class="price_comb"]').text
-      full_info = ["#{product_name}, #{product_variant}, #{product_price}"]
+      full_info = ["#{product_name} - #{v&.text}", product_price[index]&.text.to_s, product_img]
 
       csv_line << full_info
     end
-
   end
 end
